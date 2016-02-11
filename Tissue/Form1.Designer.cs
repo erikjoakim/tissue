8d7200422270ad8ea551bf37f9d324bc03df1a1d
Joakim Hasselgren <erikjoakim@gmail.com>
2016-02-11 09:38:26 +0100

    Ver 0.1. Will implement a Cell which is not binary but integer based

--- Tissue/Form1.Designer.cs
+++ Tissue/Form1.Designer.cs
@@ -38,6 +38,7 @@
             this.btnRestore = new System.Windows.Forms.Button();
             this.btnOpenFile = new System.Windows.Forms.Button();
             this.btnStepSim = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbTransferF)).BeginInit();
             this.SuspendLayout();
@@ -124,11 +125,22 @@
             this.btnStepSim.UseVisualStyleBackColor = true;
             this.btnStepSim.Click += new System.EventHandler(this.btnStepSim_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(171, 1);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Branch: Integer";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(945, 696);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.btnRestore);
             this.Controls.Add(this.btnQuickSave);
             this.Controls.Add(this.btnOpenFile);
@@ -143,6 +155,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbTransferF)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -157,6 +170,7 @@
         private System.Windows.Forms.Button btnRestore;
         private System.Windows.Forms.Button btnOpenFile;
         private System.Windows.Forms.Button btnStepSim;
+        private System.Windows.Forms.Label label1;
     }
 }
 
